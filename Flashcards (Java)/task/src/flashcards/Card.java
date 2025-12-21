dacd59f86a406624bd27d854f111261ea6a02bd3
Paweł Daniluk <pawel.daniluk@kotalbert.net>
2025-12-21 09:37:42 +0100

    Refactor Card class to use record and improve input validation for terms and definitions

--- Flashcards (Java)/task/src/flashcards/Card.java
+++ Flashcards (Java)/task/src/flashcards/Card.java
@@ -1,19 +1,4 @@
 package flashcards;
 
-public class Card {
-    private final String term;
-    private final String definition;
-
-    public Card(String term, String definition) {
-        this.term = term;
-        this.definition = definition;
-    }
-
-    public String getTerm() {
-        return term;
-    }
-
-    public String getDefinition() {
-        return definition;
-    }
+public record Card(String term, String definition) {
 }
--- Flashcards (Java)/task/src/flashcards/Main.java
+++ Flashcards (Java)/task/src/flashcards/Main.java
@@ -1,8 +1,6 @@
 package flashcards;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Scanner;
+import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
@@ -10,23 +8,45 @@ public class Main {
         System.out.println("Input the number of cards:");
         int cardsNumber = Integer.parseInt(sc.nextLine());
         List<Card> cards = new ArrayList<>();
+        Set<String> terms = new HashSet<>();
+        Set<String> definitions = new HashSet<>();
 
         for (int i = 0; i < cardsNumber; i++) {
-            System.out.println("Card #" + (i + 1) + ":");
-            String term = sc.nextLine();
-            System.out.println("The definition for card #" + (i + 1) + ":");
-            String definition = sc.nextLine();
-            Card card = new Card(term, definition);
-            cards.add(card);
+            String term, definition;
+            while (true) {
+                System.out.println("Card #" + (i + 1) + ":");
+                term = sc.nextLine();
+                if (terms.contains(term)) {
+                    System.out.printf("The term \"%s\" already exists. Try again:%n", term);
+                    continue;
+                }
+                terms.add(term);
+                break;
+            }
+            while (true) {
+
+                System.out.println("The definition for card #" + (i + 1) + ":");
+                definition = sc.nextLine();
+                if (definitions.contains(definition)) {
+                    System.out.printf("The definition \"%s\" already exists. Try again:%n", definition);
+                    continue;
+                }
+                definitions.add(definition);
+                break;
+            }
+            if (!term.isEmpty() && !definition.isEmpty()) {
+                Card card = new Card(term, definition);
+                cards.add(card);
+            }
         }
 
         for (Card card : cards) {
-            System.out.printf("Print the definition of \"%s\":%n", card.getTerm());
+            System.out.printf("Print the definition of \"%s\":%n", card.term());
             String userDefinition = sc.nextLine();
-            if (userDefinition.equals(card.getDefinition())) {
+            if (userDefinition.equals(card.definition())) {
                 System.out.println("Correct!");
             } else {
-                System.out.printf("Wrong. The right answer is \"%s\".%n", card.getDefinition());
+                System.out.printf("Wrong. The right answer is \"%s\".%n", card.definition());
             }
         }
 
