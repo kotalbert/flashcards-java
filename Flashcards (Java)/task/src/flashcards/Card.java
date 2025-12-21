068b71742c02178de8c2042858c9990bbcab2d03
Paweł Daniluk <pawel.daniluk@kotalbert.net>
2025-12-21 01:09:35 +0100

    Add multiple cards per game

---
+++ Flashcards (Java)/task/src/flashcards/Card.java
@@ -0,0 +1,19 @@
+package flashcards;
+
+public class Card {
+    private final String term;
+    private final String definition;
+
+    public Card(String term, String definition) {
+        this.term = term;
+        this.definition = definition;
+    }
+
+    public String getTerm() {
+        return term;
+    }
+
+    public String getDefinition() {
+        return definition;
+    }
+}
--- Flashcards (Java)/task/src/flashcards/Main.java
+++ Flashcards (Java)/task/src/flashcards/Main.java
@@ -1,19 +1,33 @@
 package flashcards;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.println("Card:");
-        String term = sc.nextLine();
-        System.out.println("Definition:");
-        String definition = sc.nextLine();
-        String answer = sc.nextLine();
-        if (definition.equals(answer)) {
-            System.out.println("Your answer is right!");
-        } else {
-            System.out.println("Your answer is wrong...");
+        System.out.println("Input the number of cards:");
+        int cardsNumber = Integer.parseInt(sc.nextLine());
+        List<Card> cards = new ArrayList<>();
+
+        for (int i = 0; i < cardsNumber; i++) {
+            System.out.println("Card #" + (i + 1) + ":");
+            String term = sc.nextLine();
+            System.out.println("The definition for card #" + (i + 1) + ":");
+            String definition = sc.nextLine();
+            Card card = new Card(term, definition);
+            cards.add(card);
+        }
+
+        for (Card card : cards) {
+            System.out.printf("Print the definition of \"%s\":%n", card.getTerm());
+            String userDefinition = sc.nextLine();
+            if (userDefinition.equals(card.getDefinition())) {
+                System.out.println("Correct!");
+            } else {
+                System.out.printf("Wrong. The right answer is \"%s\".%n", card.getDefinition());
+            }
         }
 
 
