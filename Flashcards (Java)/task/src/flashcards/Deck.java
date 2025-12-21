5024059e7077075f6ef6995095768455814a856d
Paweł Daniluk <pawel.daniluk@kotalbert.net>
2025-12-21 18:13:42 +0100

    Implement Deck class

---
+++ Flashcards (Java)/task/src/flashcards/Deck.java
@@ -0,0 +1,48 @@
+package flashcards;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Deck {
+    private final Map<String, String> cards;
+
+    public Deck() {
+        cards = new HashMap<>();
+    }
+
+    public void addCard(String term, String definition) {
+        if (cards.containsKey(term)) {
+            throw new IllegalArgumentException("The term \"" + term + "\" already exists. Try again.");
+        }
+        if (isDefinitionPresent(definition)) {
+            String existingTerm = getTermByDefinition(definition);
+            String errorMessage = String.format("Wrong. The right answer is \"%s\", but your definition is correct for \"%s\".", definition, existingTerm);
+            throw new IllegalArgumentException(errorMessage);
+        }
+        cards.put(term, definition);
+    }
+
+    private String getTermByDefinition(String definition) {
+        for (Map.Entry<String, String> entry : cards.entrySet()) {
+            if (entry.getValue().equals(definition)) {
+                return entry.getKey();
+            }
+        }
+        return null;
+    }
+    /**
+     * Check if definition already present in values of cards map.
+     * @param definition
+     * @return
+     */
+    private boolean isDefinitionPresent(String definition) {
+        for (String value : cards.values()) {
+            if (value.equals(definition)) {
+                return true;
+            }
+        }
+        return false;
+
+    }
+
+}
