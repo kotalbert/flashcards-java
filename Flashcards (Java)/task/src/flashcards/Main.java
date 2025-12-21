4666d420114d222df374bd8d831336b130b04155
Paweł Daniluk <pawel.daniluk@kotalbert.net>
2025-12-21 01:02:27 +0100

    Add simple guessing

--- Flashcards (Java)/task/src/flashcards/Main.java
+++ Flashcards (Java)/task/src/flashcards/Main.java
@@ -1,8 +1,21 @@
 package flashcards;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Card:");
+        String term = sc.nextLine();
+        System.out.println("Definition:");
+        String definition = sc.nextLine();
+        String answer = sc.nextLine();
+        if (definition.equals(answer)) {
+            System.out.println("Your answer is right!");
+        } else {
+            System.out.println("Your answer is wrong...");
+        }
+
 
     }
 }
